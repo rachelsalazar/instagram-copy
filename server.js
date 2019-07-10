22fa5670aa4e7cb908a8167a0dedacc967ea06f3
Rachel Salazar <ret1992@gmail.com>
2019-07-10 14:11:28 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+const users = require('./routes/api/users');
+const app = express();
+
+//Db config
+const db = require('./config/keys').mongoURI;
+mongoose.connect(db).then(() => console.log('MongoDb connected')).catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello world'));
+
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+app.use('/api/users', users);
+
+const port = 5005;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
