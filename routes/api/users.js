9d288b6fcc98709b2b652180cf6403f407a96e42
Rachel Salazar <ret1992@gmail.com>
2019-10-16 17:23:42 -0700

    finished adding login validators

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,17 +7,24 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+//Load input validation
+const validateRegisterInput = require('../../validation/register');
 
 // @route   POST api/users/register
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+    const {errors, isValid} = validateRegisterInput(req.body);
+    //Check validation
+    if (!isValid){
+        return res.status(400).json(errors);
+    }
+
     User.findOne({email: req.body.email})
         .then(user => {
             if (user) {
-                return res.status(400).json({
-                    email: 'Email already exists'
-                })
+                errors.email = 'Email already exists'
+                return res.status(400).json(errors)
             } else {
                 const avatar = gravatar.url(req.body.email, {
                     s: '200',
