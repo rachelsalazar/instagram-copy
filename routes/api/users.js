90be3a2a589383c35490776f1078ea9298052fd5
Rachel Salazar <ret1992@gmail.com>
2019-10-21 22:14:24 -0700

    login validation added

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,6 +9,7 @@ const passport = require('passport');
 
 //Load input validation
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 // @route   POST api/users/register
 // @desc    Register user
@@ -59,6 +60,12 @@ router.post('/register', (req, res) => {
 // @desc    Login user
 // @access  Public
 router.post('/login', (req, res) => {
+    const {errors, isValid} = validateLoginInput(req.body);
+    //Check validation
+    if (!isValid){
+        return res.status(400).json(errors);
+    }
+
     const email = req.body.email;
     const password = req.body.password;
 
@@ -66,9 +73,8 @@ router.post('/login', (req, res) => {
     User.findOne({email})
         .then(user => {
             if (!user){
-                return res.status(404).json({
-                    email: 'User not found'
-                });
+                errors.email = 'User not found';
+                return res.status(404).json(errors);
             }
 
             //Check password
@@ -94,7 +100,8 @@ router.post('/login', (req, res) => {
                             }
                             );
                     } else {
-                    return res.status(400).json({password: 'Password incorrect'});
+                        errors.password = 'Password incorrect';
+                        return res.status(400).json(errors);
                     }
                 })
         })
